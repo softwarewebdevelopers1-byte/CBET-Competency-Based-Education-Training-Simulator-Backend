379db5ad1085e7c7acad65393b24c3fbb7b027ce
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-04-13 09:43:39 +0300

    Added connect database connection string

--- backend/src/DatabaseRoutes/connect.ts
+++ backend/src/DatabaseRoutes/connect.ts
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 export function DataBaseConnection() {
-  let ConnectionString = process.env.MONGO_URI_LOCAL;
+  let ConnectionString = process.env.MONGO_URI;
   if (!ConnectionString) {
     console.log("MONGO_URI is not defined in environment variables");
     process.exit(1);
